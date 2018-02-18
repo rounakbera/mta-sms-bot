d82da796a37014f1e5a3cecb7c37b1dfe2340902
Ray725 <Ray725@users.noreply.github.com>
2018-02-17 23:38:10 -0500

    1 2 3 limited

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def incoming_sms():
         for j in range(len(details_list)):
             return_str += details_list[j].text
         if(subwayQuery in return_str):
-            print(return_str)
+            resp.message(return_str)
 
     return str(resp)
 
@@ -65,11 +65,12 @@ def get_subway_lines(tag_list):
 
 if __name__ == "__main__":
     # print(asdf[0].find("img").text)
-    app.run(debug=True)
+    # app.run(debug=True)
 
     '''
+    body = str(input("what's your line you want"))
     service_list = scrape_service_data('http://www.mta.info/status/subway/123/25315367')
-    subwayQuery = "3" + " " + "Subway"
+    subwayQuery = body + " " + "Subway"
     for i in range(len(service_list)):
         details_list = service_list[i].findAll("b")
         return_str = get_subway_lines(service_list[i].findAll("img"))
