ef58221fb7850667f030032912afae04034682e0
Ray725 <Ray725@users.noreply.github.com>
2018-02-18 00:10:07 -0500

    try to fix bug where it doesnt appear

--- app.py
+++ app.py
@@ -65,18 +65,4 @@ def get_subway_lines(tag_list):
 
 
 if __name__ == "__main__":
-    # print(asdf[0].find("img").text)
     app.run(debug=True)
-
-    '''
-    body = str(input("what's your line you want"))
-    service_list = scrape_service_data('http://www.mta.info/status/subway/123/25315367')
-    subwayQuery = body + " " + "Subway"
-    for i in range(len(service_list)):
-        details_list = service_list[i].findAll("b")
-        return_str = get_subway_lines(service_list[i].findAll("img"))
-        for j in range(len(details_list)):
-            return_str += details_list[j].text
-        if(subwayQuery in return_str):
-            print(return_str)
-    '''
