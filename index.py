3b5fbebdebf1beb784455ada61ab9d6563d8d0f6
Ray725 <Ray725@users.noreply.github.com>
2018-02-17 21:58:45 -0500

    start testing twilio message reply

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
 # Sending an SMS using the Twilio API
 from twilio.rest import Client
 import requests
+from bs4 import BeautifulSoup
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
 # put your own credentials here
 account_sid = "AC8816577cc14eec0b84cb97298837edb9"
 auth_token = "f74851e59b188f810f96afaac52eabfe"
@@ -16,7 +22,22 @@ client.messages.create(
 '''
 
 def scrape_data():
-    r = requests.get('http://www.mta.info/')
-    print(r.status_code)
+    r = requests.get('http://www.mta.info/status/subway/123/25315367')
+    soup = BeautifulSoup(r.text, 'html.parser')
+    service_list = soup.findAll("a", {"class": "plannedWorkDetailLink"})
+    # print(service_list[0].find("i"))
+    return service_list
 
-scrape_date()
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
