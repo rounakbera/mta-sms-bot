128f5964b5a777d7fdcaaff2373922ff36446c84
Ray725 <Ray725@users.noreply.github.com>
2018-02-17 22:46:53 -0500

    first attempt at data scrape

--- app.py
+++ app.py
@@ -7,18 +7,12 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+'''
 # put your own credentials here
 account_sid = "AC8816577cc14eec0b84cb97298837edb9"
 auth_token = "f74851e59b188f810f96afaac52eabfe"
 
 client = Client(account_sid, auth_token)
-
-'''
-client.messages.create(
-  to="+13142584515",
-  from_="+16463928126",
-  body="FUCK YOU ROUNAK",
-  media_url="https://climacons.herokuapp.com/clear.png")
 '''
 
 def scrape_data():
@@ -43,9 +37,27 @@ def incoming_sms():
 
     # Determine the right reply for this message
     if body == '1':
-        resp.message("scrape_data()")
+        service_list = scrape_data()
+        resp.message(scrape_data()[2].findAll("img"))
+        for i in service_list:
+            message = Message()
+            message.body = get_subway_lines(scrape_data()[i].findAll("img")) + " " + service_list[i].find("i").text
+            resp.append(message)
 
     return str(resp)
+
+def get_subway_lines(tag_list):
+    return_str = ""
+    for i in range(len(tag_list)):
+        return_str += tag_list[i]["alt"] + " "
+    return return_str
+
+'''
+print(scrape_data()[0].find("i").text)
+print(scrape_data()[0].findAll("img"))
+print(get_subway_lines(scrape_data()[0].findAll("img")))
+'''
+
 
 if __name__ == "__main__":
     app.run(debug=True)
