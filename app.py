a3e120b91d38f75cd25a3218ff15a4703ca32de9
Ray725 <Ray725@users.noreply.github.com>
2018-02-17 23:34:12 -0500

    account for different subway lines

--- app.py
+++ app.py
@@ -44,33 +44,15 @@ def incoming_sms():
     resp = MessagingResponse()
 
     # Determine the right reply for this message
-    if body == '1':
-        service_list = scrape_service_data('http://www.mta.info/status/subway/123/25315367')
-        for i in range(len(service_list)):
-            details_list = service_list[i].findAll("b")
-            return_str = get_subway_lines(service_list[i].findAll("img"))
-            for j in range(len(details_list)):
-                return_str += details_list[j].text
-            if(return_str[0] == '1'):
-                resp.message(return_str)
-    else if body == '2':
-        service_list = scrape_service_data('http://www.mta.info/status/subway/123/25315367')
-        for i in range(len(service_list)):
-            details_list = service_list[i].findAll("b")
-            return_str = get_subway_lines(service_list[i].findAll("img"))
-            for j in range(len(details_list)):
-                return_str += details_list[j].text
-            if(return_str[0] == '2'):
-                resp.message(return_str)
-    else if body == '3':
-        service_list = scrape_service_data('http://www.mta.info/status/subway/123/25315367')
-        for i in range(len(service_list)):
-            details_list = service_list[i].findAll("b")
-            return_str = get_subway_lines(service_list[i].findAll("img"))
-            for j in range(len(details_list)):
-                return_str += details_list[j].text
-            if(return_str[0] == '3'):
-                resp.message(return_str)
+    service_list = scrape_service_data('http://www.mta.info/status/subway/123/25315367')
+    subwayQuery = body + " " + "Subway"
+    for i in range(len(service_list)):
+        details_list = service_list[i].findAll("b")
+        return_str = get_subway_lines(service_list[i].findAll("img"))
+        for j in range(len(details_list)):
+            return_str += details_list[j].text
+        if(subwayQuery in return_str):
+            print(return_str)
 
     return str(resp)
 
@@ -87,10 +69,12 @@ if __name__ == "__main__":
 
     '''
     service_list = scrape_service_data('http://www.mta.info/status/subway/123/25315367')
+    subwayQuery = "3" + " " + "Subway"
     for i in range(len(service_list)):
         details_list = service_list[i].findAll("b")
         return_str = get_subway_lines(service_list[i].findAll("img"))
         for j in range(len(details_list)):
             return_str += details_list[j].text
-        print(return_str)
+        if(subwayQuery in return_str):
+            print(return_str)
     '''
